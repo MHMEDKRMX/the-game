c0d28b9e0623d81bc041c57688b346b226f21e22
MHMEDKRMX <wsn90125xx@gmail.com>
2024-10-09 17:53:34 +0300

    برررررررر

---
+++ the game/Assets/Script/movement.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingpower = 16f;
+    private bool isfacingright;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundcheck;
+    [SerializeField] private LayerMask groundlayer;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        flip();
+
+        if(Input.GetButtonDown("Jump") && isgrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingpower);
+        }
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private bool isgrounded()
+    {
+        return Physics2D.OverlapCircle(groundcheck.position,0.2f,groundlayer);
+    }
+    
+
+
+    private void flip()
+    {
+        if (isfacingright && horizontal < 0f || !isfacingright && horizontal > 0f) 
+        {
+
+            isfacingright = !isfacingright;
+            Vector3 localscale = transform.localScale;
+            localscale.x *= -1f;
+            transform.localScale = localscale;
+
+
+
+
+        }
+    }
+
+
+
+}
+
+
